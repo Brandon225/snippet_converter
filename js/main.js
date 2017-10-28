7ae658240a3039c2f40e9a9e46a411b201a510d6
Brandon Tindle <bjtactor@gmail.com>
2017-10-27 23:18:38 -0400

    Bootstrap migration Detail complete!

--- js/main.js
+++ js/main.js
@@ -471,7 +471,7 @@ $('[type="submit"]').mouseenter(function()
 
 
 /*Smooth link animation*/
-$('a[href*=#]:not([href=#])').click(function() {
+$('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
         var target = $(this.hash);
