1b5d6cf5a17d6f62ae638bb4a439712f266292a4
Brandon Tindle <bjtactor@gmail.com>
2017-12-12 23:51:55 -0500

    Added favicons.

--- includes/head.php
+++ includes/head.php
@@ -20,6 +20,14 @@
 <meta name="twitter:description" content="A snippet converter. Moving to a new code editor?  Convert your code snippets quickly and easily!" />
 <meta name="twitter:image" content="http://snippets.reimagin8d.com/img/logo.png" />
 
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#d85360">
+<meta name="theme-color" content="#ffffff">
+
 <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
--- templates/about.php
+++ templates/about.php
@@ -23,13 +23,18 @@
             <div class="row">
                 <div class='col-sm-6'>
                     <img class="img-thumbnail" src="/img/me.jpg" alt="Brandon Tindle Developer Designer">
+                    <!-- <ul class="list-unstyled mt-4">
+                        Contact
+                        <hr class="my-1">
+                        <li><a class="text-black hover-red" href="mailto:reimagin8d@reimagin8d.com?subject=Contact%20Inquiry">Email</a></li>
+                        <li><a class="text-black hover-red" href="https://www.upwork.com/o/profiles/users/_~013867cd1e9a262919/" target="_blank">Upwork</a></li>
+                        <li><a class="text-black hover-red" href="www.linkedin.com/in/brandon-tindle" target="_blank">LinkedIn</a></li>
+                    </ul> -->
                 </div>
                 <div class='col-sm-6'>
                     <p>Clean, simple and scalable are the hallmarks of my work. Maintaining clean, simple and scalable code allows me to build stable, sustainable applications.</p>
 
                     <p>I am a FULL STACK developer &amp; designer with over eight years of experience building and designing websites, databases, servers, and mobile applications. I have a BFA from The Boston Conservatory. Unsatisfied with the quality of applications built for the theater world, I took matters into my own hands and delved head first into Mobile and Web Design and Development.</p>
-
-                    <!-- <p>I completed Stanford University’s iPhone Development courses via iTunes U and devoured every Objective-C book I could find. I now have five applications on the App Store, three of which have been featured by Apple.</p> -->
 
                     <p>In 2013, I was awarded the position of Software Project Manager at the prestigious New York Vocal Coaching where I accumulated over 2,000 hours building and designing websites, mobile applications, logo design and branding, and graphics design.</p>
 
@@ -37,6 +42,13 @@
 
 
                     <p>This all speaks to my abilities to quickly learn and properly execute new methods and ideas.</p>
+                    <ul class="list-unstyled mt-4">
+                        Contact
+                        <hr class="my-1">
+                        <li><a class="text-black hover-red" href="mailto:reimagin8d@reimagin8d.com?subject=Contact%20Inquiry">Email</a></li>
+                        <li><a class="text-black hover-red" href="https://www.upwork.com/o/profiles/users/_~013867cd1e9a262919/" target="_blank">Upwork</a></li>
+                        <li><a class="text-black hover-red" href="www.linkedin.com/in/brandon-tindle" target="_blank">LinkedIn</a></li>
+                    </ul>
                 </div>
             </div>
 
